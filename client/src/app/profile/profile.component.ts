c056007725ecf92baacd979c9a9639f7f3fb00d4
Anji <guntianjaneyulu05@gmail.com>
2018-03-07 23:59:02 +0530

    server-setup

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  private infoTab : boolean;
+  private feedsTab : boolean;
+  private mediaTab : boolean;
+  isClassVisible: false;
+
+  
+  constructor() { 
+    this.infoTab = true;
+    this.feedsTab = false;
+    this.mediaTab = false;
+  }
 
   ngOnInit() {
+    this.infoTab = true;
   }
+
+  showProfileTabs(id : string, event){
+    switch(id){
+      case 'infoT' :
+      this.infoTab = true;
+      this.feedsTab = false;
+      this.mediaTab = false;
+      event.target.classList.add('ptab_active');
+      break;
+
+      case 'feedT' :
+      this.infoTab = false;
+      this.feedsTab = true;
+      this.mediaTab = false;
+      event.target.classList.add('ptab_active');
+      break;
+
+      case 'mediaT' :
+      this.infoTab = false;
+      this.feedsTab = false;
+      this.mediaTab = true;
+      event.target.classList.add('ptab_active');
+      break;
+
+      case 'defualt' :
+      this.infoTab = true;
+      break;
+    }  
+}
 
 }
